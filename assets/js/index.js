e1942e511105639f3660b9751cf8275933a4f2e5
RoniPG <ronigmn@gmail.com>
2025-11-15 01:22:03 +0100

    Complete Projects section && add Certifications section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,28 @@
 const carousel = document.getElementById("carousel");
 
-// Make infinite scroll
 function adjustInfiniteScroll() {
     const items = carousel.children;
-    const itemWidth = items[0].offsetWidth + 14; // ancho + gap
+    console.log('items[0].offsetWidth', items[0].offsetWidth);
+    const itemsWidth = items[0].offsetWidth;
+    console.log('carousel.scrollLeft',carousel.scrollLeft );
+    console.log('carousel.scrollWidth',carousel.scrollWidth );
+    console.log(carousel.clientWidth);
+    console.log(carousel.offsetWidth);
+    console.log(carousel.scrollWidth - carousel.clientWidth);
+    
+    
+    
+    
+console.log('Math.floor(carousel.scrollLeft)', Math.floor(carousel.scrollLeft));
+console.log('Math.ceil(carousel.scrollLeft)', Math.ceil(carousel.scrollLeft));
 
-
-    if (carousel.scrollLeft <= 0) {
-        carousel.scrollLeft = itemWidth * (items.length - 2);
-    }
-
-
-    if (carousel.scrollLeft >= itemWidth * (items.length - 1)) {
-        carousel.scrollLeft = itemWidth;
+    if (Math.floor(carousel.scrollLeft) <= 1 ) {
+        carousel.scrollLeft = carousel.scrollWidth - carousel.clientWidth - 2
+    } 
+    
+    if (Math.ceil(carousel.scrollLeft) >= carousel.scrollWidth - carousel.clientWidth) {
+        carousel.scrollLeft = 2
     }
 }
-
 
 carousel.addEventListener("scroll", adjustInfiniteScroll);
