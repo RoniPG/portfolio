056f061af1a9a37949f41e762ef1c904797f0d72
RoniPG <ronigmn@gmail.com>
2025-11-15 18:19:14 +0100

    Mvp

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,16 @@
-const carousel = document.getElementById("carousel");
+const techCarousel = document.getElementById("tech-carousel");
 
-function adjustInfiniteScroll() {
+function adjustInfiniteScroll(carousel) {
     const items = carousel.children;
-    console.log('items[0].offsetWidth', items[0].offsetWidth);
     const itemsWidth = items[0].offsetWidth;
-    console.log('carousel.scrollLeft',carousel.scrollLeft );
-    console.log('carousel.scrollWidth',carousel.scrollWidth );
-    console.log(carousel.clientWidth);
-    console.log(carousel.offsetWidth);
-    console.log(carousel.scrollWidth - carousel.clientWidth);
-    
-    
-    
-    
-console.log('Math.floor(carousel.scrollLeft)', Math.floor(carousel.scrollLeft));
-console.log('Math.ceil(carousel.scrollLeft)', Math.ceil(carousel.scrollLeft));
 
-    if (Math.floor(carousel.scrollLeft) <= 1 ) {
+    if (Math.floor(carousel.scrollLeft) <= 1) {
         carousel.scrollLeft = carousel.scrollWidth - carousel.clientWidth - 2
-    } 
-    
+    }
+
     if (Math.ceil(carousel.scrollLeft) >= carousel.scrollWidth - carousel.clientWidth) {
         carousel.scrollLeft = 2
     }
 }
 
-carousel.addEventListener("scroll", adjustInfiniteScroll);
+techCarousel.addEventListener("scroll", () => adjustInfiniteScroll(techCarousel));
